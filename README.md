5b6822e24ab0a7091d4a71ce4b92c27b779ad941
Simbiat <simbiat@outlook.com>
2021-03-30 18:37:14 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # HTMLCut
- Class to cut HTML text into something suitable for preview
+This is a class to cut HTML text while preserving (to an extent) HTML structure. Can be used to create previews of articles, written in HTML and stored in database, for example. Will work just as well with regular text.
+
+## Why?
+This class has some benefits:
+1. Preserve HTML tags, unless they are empty.
+2. Preserve words.
+3. Remove some orphaned punctuation signs at the end of the cut string.
+4. Remove HTML tags, that you would not want in a preview (optional).
+5. Limit number of paragraphs (optional).
+6. Add an ellipsis if text was cut (optional).
+
+## Details on usage
+```php
+Cut(\DOMNode|string $string, int $length, int $paragraphs = 0, string $ellipsis = '…', bool $stripUnwated = true)
+```
+`$string` is the text you want to cut. This argument also accepts `\DOMNode` objects, but it's not intended, that you will send them to it: this is simply a requirement due to recursive nature of the function.  
+`$length` is the expected maximum of the resulting text. Note, that due to words preservation result **may** be a bit longer, but normally, not by much.  
+`$paragraphs` is the maximum number of paragraphs in the result. In this case `paragraph` means not only text with following new line (or end of string) and `<p>` tags, but also some other HTML tags, that are generally shown as if a new paragraph, like `<li>`. This is useful if you want to limit the number of `lines` shown in your previews. Generally, cutting by characters should be enough, but if the content you are sharing starts with multiple short lines (for example, a poem), this may result in a preview, that will be longer, than the rest. Setting this to a value more than `0` will help prevent that. List of tags to treat as paragraphs can be edited before calling the fucntion by directly modifying `$paraTags` class variable.  
+`$ellipsis` is an optional text, that you want to display after the cut text. Normally you would want this to be a link like "Read More" or something else. Defaults to unicode vertical lower ellipsis symbol `…` (not `...`).  
+`$stripUnwated` if set to `true` will remove any tags, that you may not want to be shown in preview, like images and tables. List of tags to remove can by directly modified by editing `$extraTags` class variable.
---
+++ src/HTMLCut.php
@@ -0,0 +1,154 @@
+<?php
+declare(strict_types=1);
+namespace Simbiat;
+
+class HTMLCut
+{
+    #Tags, that we consider irrelavant or harmful for preview
+    public array $extraTags = [
+        'applet', 'area', 'audio', 'base', 'blockquote', 'body', 'button', 'canvas', 'code', 'col', 'data', 'datalist', 'details', 'dialog', 'dir', 'embed', 'fieldset', 'figcapture', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'header', 'html', 'iframe', 'img', 'input', 'ins', 'kbd', 'legend', 'link', 'main', 'map', 'meta', 'nav', 'noframes', 'noscript', 'object', 'optgroup', 'option', 'output', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 'samp', 'script', 'select', 'source', 'style', 'summary', 'svg', 'table', 'tbody', 'td', 'tempalte', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'track', 'tt', 'var', 'video',
+    ];
+    #Tags that we consider paragpraphs
+    public array $paraTags = [
+        'article', 'aside', 'div', 'li', 'p', 'section',
+    ];
+    #Regex to remove punctuation symbols from the end of the string, that may make no sense there
+    private string $punctuation = '/([:;,\[\(\-\{\<_„“‘«「﹁‹『﹃《〈]{1,}|\.{2,})$/ui';
+    
+    public function Cut(\DOMNode|string $string, int $length, int $paragraphs = 0, string $ellipsis = '…', bool $stripUnwated = true): \DOMNode|string
+    {
+        #Sanitize length
+        if ($length < 0) {
+            $length = 0;
+        }
+        #Sanitize paragraphs
+        if ($paragraphs < 0) {
+            $paragraphs = 0;
+        }
+        if (is_string($string)) {
+            #Check if string is too long without HTML tags
+            if (mb_strlen(strip_tags($string), 'UTF-8') > $length) {
+                #Convert to HTML DOM object
+                $html = new \DOMDocument();
+                #LIBXML_HTML_NOIMPLIED and LIBXML_HTML_NODEFDTD to avoid adding wrappers (html, body, DTD). This will also allow less issues in case string has both regular HTML and some regular text (outside of any tags). LIBXML_NOBLANKS to remove empty tags if any. LIBXML_PARSEHUGE to allow processing of larger strings.
+                $html->loadHTML($string, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NOBLANKS | LIBXML_PARSEHUGE);
+                $html->preserveWhiteSpace = false;
+                $html->formatOutput = false;
+                $html->normalizeDocument();
+            }
+        } else {
+            #We already have a DOMNode
+            $html = $string;
+        }
+        #Check of node has children
+        if (count($html->childNodes) > 0) {
+            #Set new length
+            $newLength = 0;
+            #Iterrate children. While theoretically we can use the getElementsByTagName (as is also done further down the code), I was not able to get consistent results with it on this step, often not getting any text whatsoever.
+            foreach ($html->childNodes as $key=>$node) {
+                #Get length of current node
+                $nodeLength = mb_strlen(strip_tags($node->nodeValue), 'UTF-8');
+                #Check if it fits
+                if ($newLength + $nodeLength <= $length) {
+                    #Increase current length
+                    $newLength += $nodeLength;
+                    #Go to next node
+                    continue;
+                } else {
+                    #Need to cut the value
+                    #Check if DOMText
+                    if ($node instanceof \DOMText) {
+                        #Cut directly in the DOM. Regex allows to retain whole words.
+                        $html->childNodes->item($key)->nodeValue = preg_replace('/^(.{'.(($length - $newLength) > 0 ? '1,'.($length - $newLength) : '0,0').'}\b)(.*)/siu', '$1', $html->childNodes->item($key)->nodeValue);
+                    } else {
+                        #Recurse and replace current node with new (possibly cut) node
+                        $html->replaceChild($this->Cut($node, $length - $newLength), $node);
+                    }
+                    #Get length of updated node
+                    $nodeLength = mb_strlen(strip_tags($html->childNodes->item($key)->nodeValue), 'UTF-8');
+                    if ($newLength + $nodeLength <= $length) {
+                        #Update current length
+                        $newLength += $nodeLength;
+                    } else {
+                        #Remove child, since its excessive
+                        $node->parentNode->removeChild($node);
+                    }
+                }
+            }
+        } else {
+            #Check if what we have is already a \DOMText
+            if ($html instanceof \DOMText) {
+                #Cut directly in the DOM. Regex allows to retain whole words.
+                $html->nodeValue = preg_replace('/^(.{'.(($length - $newLength) > 0 ? '1,'.($length - $newLength) : '0,0').'}\b)(.*)/siu', '$1', $html->nodeValue);
+            }
+        }
+        if ($html instanceof \DOMDocument) {
+            #Set xpath variable
+            $xpath = new \DOMXPath($html);
+            #Remove all tags, that do not make sense or have potential to harm in a preview
+            if ($stripUnwated) {
+                foreach ($xpath->query(implode('|', array_map(function($val) { return '//'.$val;} , $this->extraTags))) as $node) {
+                    $node->parentNode->removeChild($node);
+                }
+            }
+            #Reduce number of paragraphs shown
+            if ($paragraphs > 0) {
+                #Get current number of paragraphs. Also counting other elements, that generally look as separate paragraphs.
+                $curPar = $xpath->query(implode('|', array_map(function($val) { return '//'.$val;} , $this->paraTags)))->length;
+                #Check if number of current paragraphs is larger than allowed. Do not do processing, if it's not.
+                if ($curPar > $paragraphs) {
+                    #Get all tags
+                    $tags = $html->getElementsByTagName('*');
+                    #Iterrate backwards (as per https://www.php.net/manual/en/class.domnodelist.php#83390). Regular iterration seems to provide strange results.
+                    for ($i = $tags->length; --$i >= 0;) {
+                        #Check if number of current paragraphs is larger than allowed
+                        if ($curPar > $paragraphs) {
+                            if (in_array(strtolower($node->nodeName), $this->paraTags)) {
+                                $curPar--;
+                            }
+                            #Get actual node
+                            $node = $tags->item($i);
+                            #Remove node
+                            $node->parentNode->removeChild($node);
+                        }
+                    }
+                }
+            }
+            #Remove all empty nodes (taken from https://stackoverflow.com/questions/40367047/remove-all-empty-html-elements-using-php-domdocument). Using `while` allows for recursion
+            while (($node_list = $xpath->query('//*[not(*) and not(@*) and not(text()[normalize-space()])]')) && $node_list->length) {
+                foreach ($node_list as $node) {
+                    $node->parentNode->removeChild($node);
+                }
+            }
+            #Update string
+            $newString = $html->saveHTML();
+        } else {
+            return $html;
+        }
+        #Check if string got updated
+        if (isset($newString)) {
+            $string = $newString;
+        }
+        #Reduce number of paragraphs shown. While this has been done in terms of pure HTML, there is a chance, that we have regular text with regular newlines.
+        if ($paragraphs > 0) {
+            #Remove any whitespace between HTML tags, newlines before/after tags and also trim (as precaution)
+            $string = trim(preg_replace('/(>|<)(\r+\n+|\r+|\n+)/i', '$1', preg_replace('/(\r+\n+|\r+|\n+)(>|<)/i', '$2', preg_replace('/>\s+</m', '><', $string))));
+            #Explode by newlines  (treat multiple newlines as one)
+            $curPar = preg_split('/\r+\n+|\r+|\n+/', $string);
+            if (count($curPar) > $paragraphs) {
+                #Slice and then implode back
+                $newString = implode('<br>', array_slice($curPar, 0, $paragraphs));
+            }
+        }
+        #Return
+        if (isset($newString)) {
+            #Remove some common punctuation from the end of the string (if any). These elements, when found ad the end of string, may look out of place. Also remove any excessive <br> at the beginning and end of the string.
+            $string = preg_replace('/(^(\<br\>)+)|((\<br\>)+$)/i', '', preg_replace($this->punctuation, '', $newString));
+            #Return with ellipsis
+            return $string.$ellipsis;
+        } else {
+            return $string;
+        }
+    }
+}
+?>
