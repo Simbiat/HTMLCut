029db959bb05ce4df8124e76f0de58979fe7d940
Simbiat <simbiat@outlook.com>
2021-05-20 18:35:19 +0300

    Styling and minor adjustments

--- src/HTMLCut.php
+++ src/HTMLCut.php
@@ -4,18 +4,18 @@ namespace Simbiat;
 
 class HTMLCut
 {
-    #Tags, that we consider irrelavant or harmful for preview
+    #Tags, that we consider irrelevant or harmful for preview
     public array $extraTags = [
-        'applet', 'area', 'audio', 'base', 'blockquote', 'body', 'button', 'canvas', 'code', 'col', 'data', 'datalist', 'details', 'dialog', 'dir', 'embed', 'fieldset', 'figcapture', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'header', 'html', 'iframe', 'img', 'input', 'ins', 'kbd', 'legend', 'link', 'main', 'map', 'meta', 'nav', 'noframes', 'noscript', 'object', 'optgroup', 'option', 'output', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 'samp', 'script', 'select', 'source', 'style', 'summary', 'svg', 'table', 'tbody', 'td', 'tempalte', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'track', 'tt', 'var', 'video',
+        'applet', 'area', 'audio', 'base', 'blockquote', 'body', 'button', 'canvas', 'code', 'col', 'data', 'datalist', 'details', 'dialog', 'dir', 'embed', 'fieldset', 'figcapture', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'header', 'html', 'iframe', 'img', 'input', 'ins', 'kbd', 'legend', 'link', 'main', 'map', 'meta', 'nav', 'noframes', 'noscript', 'object', 'optgroup', 'option', 'output', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 'samp', 'script', 'select', 'source', 'style', 'summary', 'svg', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'track', 'tt', 'var', 'video',
     ];
-    #Tags that we consider paragpraphs
+    #Tags that we consider paragraphs
     public array $paraTags = [
         'article', 'aside', 'div', 'li', 'p', 'section',
     ];
     #Regex to remove punctuation symbols from the end of the string, that may make no sense there
     private string $punctuation = '/([:;,\[\(\-\{\<_„“‘«「﹁‹『﹃《〈]{1,}|\.{2,})$/ui';
-    
-    public function Cut(\DOMNode|string $string, int $length, int $paragraphs = 0, string $ellipsis = '…', bool $stripUnwated = true): \DOMNode|string
+
+    public function Cut(\DOMNode|string $string, int $length, int $paragraphs = 0, string $ellipsis = '…', bool $stripUnwanted = true): \DOMNode|string
     {
         #Sanitize length
         if ($length < 0) {
@@ -31,7 +31,7 @@ class HTMLCut
                 #Convert to HTML DOM object
                 $html = new \DOMDocument(encoding: 'UTF-8');
                 #mb_convert_encoding is done as per workaround for UTF-8 loss/corruption on load from https://stackoverflow.com/questions/8218230/php-domdocument-loadhtml-not-encoding-utf-8-correctly
-                #LIBXML_HTML_NOIMPLIED and LIBXML_HTML_NODEFDTD to avoid adding wrappers (html, body, DTD). This will also allow less issues in case string has both regular HTML and some regular text (outside of any tags). LIBXML_NOBLANKS to remove empty tags if any. LIBXML_PARSEHUGE to allow processing of larger strings. LIBXML_COMPACT for some potential optimization. LIBXML_NOWARNING and LIBXML_NOERROR to supress warning in case of malformed HTML. LIBXML_NONET to protect from unsolicited connections to external sources.
+                #LIBXML_HTML_NOIMPLIED and LIBXML_HTML_NOTED to avoid adding wrappers (html, body, DTD). This will also allow less issues in case string has both regular HTML and some regular text (outside of any tags). LIBXML_NOBLANKS to remove empty tags if any. LIBXML_PARSEHUGE to allow processing of larger strings. LIBXML_COMPACT for some potential optimization. LIBXML_NOWARNING and LIBXML_NOERROR to suppress warning in case of malformed HTML. LIBXML_NONET to protect from unsolicited connections to external sources.
                 $html->loadHTML(mb_convert_encoding($string, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD | LIBXML_NOBLANKS | LIBXML_PARSEHUGE | LIBXML_COMPACT | LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_NONET);
                 $html->preserveWhiteSpace = false;
                 $html->formatOutput = false;
@@ -42,10 +42,10 @@ class HTMLCut
             $html = $string;
         }
         if (isset($html)) {
+            #Set new length
+            $newLength = 0;
             #Check of node has children
             if (count($html->childNodes) > 0) {
-                #Set new length
-                $newLength = 0;
                 #Iterrate children. While theoretically we can use the getElementsByTagName (as is also done further down the code), I was not able to get consistent results with it on this step, often not getting any text whatsoever.
                 foreach ($html->childNodes as $key=>$node) {
                     #Get length of current node
@@ -55,7 +55,6 @@ class HTMLCut
                         #Increase current length
                         $newLength += $nodeLength;
                         #Go to next node
-                        continue;
                     } else {
                         #Need to cut the value
                         #Check if DOMText
@@ -88,7 +87,7 @@ class HTMLCut
                 #Set xpath variable
                 $xpath = new \DOMXPath($html);
                 #Remove all tags, that do not make sense or have potential to harm in a preview
-                if ($stripUnwated) {
+                if ($stripUnwanted) {
                     foreach ($xpath->query(implode('|', array_map(function($val) { return '//'.$val;} , $this->extraTags))) as $node) {
                         $node->parentNode->removeChild($node);
                     }
@@ -101,15 +100,15 @@ class HTMLCut
                     if ($curPar > $paragraphs) {
                         #Get all tags
                         $tags = $html->getElementsByTagName('*');
-                        #Iterrate backwards (as per https://www.php.net/manual/en/class.domnodelist.php#83390). Regular iterration seems to provide strange results.
+                        #Iterrate backwards (as per https://www.php.net/manual/en/class.domnodelist.php#83390). Regular iteration seems to provide strange results.
                         for ($i = $tags->length; --$i >= 0;) {
                             #Check if number of current paragraphs is larger than allowed
                             if ($curPar > $paragraphs) {
+                                #Get actual node
+                                $node = $tags->item($i);
                                 if (in_array(strtolower($node->nodeName), $this->paraTags)) {
                                     $curPar--;
                                 }
-                                #Get actual node
-                                $node = $tags->item($i);
                                 #Remove node
                                 $node->parentNode->removeChild($node);
                             }
@@ -135,7 +134,7 @@ class HTMLCut
         #Reduce number of paragraphs shown. While this has been done in terms of pure HTML, there is a chance, that we have regular text with regular newlines.
         if ($paragraphs > 0) {
             #Remove any whitespace between HTML tags, newlines before/after tags and also trim (as precaution)
-            $string = trim(preg_replace('/(>|<)(\r+\n+|\r+|\n+)/iu', '$1', preg_replace('/(\r+\n+|\r+|\n+)(>|<)/iu', '$2', preg_replace('/>\s+</mu', '><', $string))));
+            $string = trim(preg_replace('/([><])(\r+\n+|\r+|\n+)/iu', '$1', preg_replace('/(\r+\n+|\r+|\n+)([><])/iu', '$2', preg_replace('/>\s+</mu', '><', $string))));
             #Explode by newlines  (treat multiple newlines as one)
             $curPar = preg_split('/\r+\n+|\r+|\n+/u', $string);
             if (count($curPar) > $paragraphs) {
@@ -146,7 +145,7 @@ class HTMLCut
         #Return
         if (isset($newString)) {
             #Remove some common punctuation from the end of the string (if any). These elements, when found ad the end of string, may look out of place. Also remove any excessive <br> at the beginning and end of the string.
-            $string = preg_replace('/(^(\<br\>)+)|((\<br\>)+$)/iu', '', preg_replace($this->punctuation, '', $newString));
+            $string = preg_replace('/(^(<br>)+)|((<br>)+$)/iu', '', preg_replace($this->punctuation, '', $newString));
             #Return with ellipsis
             return nl2br($string.$ellipsis);
         } else {
@@ -154,4 +153,3 @@ class HTMLCut
         }
     }
 }
-?>
